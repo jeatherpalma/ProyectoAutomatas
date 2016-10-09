1a37ab43628e857942947070b96e2abc0d677bf1
Jeather <loweroner33@gmail.com>
2016-10-08 21:01:35 -0500

    Se modifico la clase Analisis_Lexico
    Se está agregando la clase del analisis lexico
    
    Puede contener errores

--- src/Clases/Analisis_Lexico.java
+++ src/Clases/Analisis_Lexico.java
@@ -9,6 +9,23 @@ public class Analisis_Lexico {
 
     //Variable terminal
     int token_terminal =0;
+    //Objeto de la pila
+    Pila obje1_pila;
+    
+    
+    
+    public int get_token_terminal()
+    {
+        int terminal = 0;
+        obje1_pila = new Pila(32);
+        
+        
+        
+        
+        return terminal;
+        
+    }
+            
     
 
 
